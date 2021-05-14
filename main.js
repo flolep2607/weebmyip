06216f60a1e9963ff93a80b307845b63639a69e8
flolep2607 <flo.lep2607@gmail.com>
2021-05-14 14:08:42 +0200

    auto commit

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ const ip2sound=(ip,res)=>{
 //	concatenater.pipe(fs.createWriteStream(`./static/generated/${btf_ip}.mp3`));
 	var FILES=ip.split(".").map(r=>`./audio/nums/${r}.mp3`)
 	FILES=insertion(FILES)
-	console.log(["./audio/phrases/baka.mp3",...FILES]);
+	console.log(["./audio/phrases/baka.mp3",YIPIS[Math.floor(Math.random() * YIPIS.length)],...FILES]);
 	audioconcat(["./audio/phrases/baka.mp3",YIPIS[Math.floor(Math.random() * YIPIS.length)],...FILES])
 		.concat(`./static/generated/${btf_ip}.mp3`)
 		.on('end', function (output) {
