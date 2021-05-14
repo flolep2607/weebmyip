b7be5f089c721ea084bad09736497a48abdd6b3f
flolep2607 <flo.lep2607@gmail.com>
2021-05-14 15:55:49 +0200

    auto commit

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ app.use((req,res,next)=>{
 	req.IP=req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 	next()
 })
-app.get('/myip.mp3',(req,res)=>{
+app.get(/\/*\.mp3/,(req,res)=>{
        ip2sound(req.IP,res)
 })
 
