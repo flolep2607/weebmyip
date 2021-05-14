fa63e7e9f1a88d60d27c40e99030c66bc62ddeae
flolep2607 <flo.lep2607@gmail.com>
2021-05-14 12:06:57 +0200

    8

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const ip2sound=(ip,res)=>{
 	concatenater = concatstream();
 	//concatenater.pipe(res)
 	concatenater.pipe(fs.createWriteStream('concat.mp3'));
-	const FILES=ip.split(".").map(r=>"./audio/nums/"+r)
+	const FILES=ip.split(".").map(r=>`./audio/nums/${r}.mp3`)
 	console.log(FILES);
 	async.eachSeries(["./audio/phrases/baka.mp3",...FILES], function(file, cb) {
 	  // ... and pipe them into the concatenater
